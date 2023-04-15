0b306bc642aadbdf785eef11ec326902d6536b24
michaelocenar <affectuminflamma@gmail.com>
2023-04-15 17:31:47 -0600

    add google maps api to itinerary.js

--- pages/itinerary.js
+++ pages/itinerary.js
@@ -1,5 +1,19 @@
 import { useRouter } from "next/router";
 import styles from "./itinerary.module.css";
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+
+const mapContainerStyle = {
+  width: '100%',
+  height: '400px',
+};
+
+const center = {
+  lat: 40.7128,
+  lng: -74.0060,
+};
+
 
 export default function Itinerary() {
   const router = useRouter();
@@ -27,6 +41,13 @@ export default function Itinerary() {
       return (
         <div className={styles.container}>
         <h1>Your Travel Itinerary</h1>
+        <LoadScript googleMapsApiKey={API_KEY}>
+          <GoogleMap
+            mapContainerStyle={mapContainerStyle}
+            zoom={12}
+            center={center}
+          />
+        </LoadScript>
         <div className={styles.tiles}>{itineraryItems}</div>
       </div>
     );
