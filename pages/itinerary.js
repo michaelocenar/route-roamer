ddc1caea6e9939531cc7fcf62ff447397cddb3ca
michaelocenar <affectuminflamma@gmail.com>
2023-04-18 00:05:52 -0600

    Remove lat and lng from text

--- pages/itinerary.js
+++ pages/itinerary.js
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import styles from "./itinerary.module.css";
-import { GoogleMap, LoadScript, Marker, InfoWindow } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 
@@ -9,86 +9,63 @@ const mapContainerStyle = {
   height: "400px",
 };
 
-const defaultCenter = {
-  lat: 40.7128,
-  lng: -74.0060,
-};
+function parseItinerary(itineraryText) {
+  const itineraryData = JSON.parse(itineraryText);
+  return itineraryData.Itinerary;
+}
 
 export default function Itinerary() {
   const router = useRouter();
   const { result } = router.query;
+  console.log("result:", result);
 
   if (typeof result === "string") {
-    const parsedResult = JSON.parse(result);
-    const itineraryItems = parsedResult.Itinerary.map((item, index) => {
-      const { Date, Activities } = item;
-
-      return (
-        <div key={index} className={styles.day}>
-          <h2>Day {index + 1}: {Date}</h2>
-          <ul className={styles.itinerary}>
-            {Activities.map((activity, index) => {
-              const { Time, Activity, Location, Description } = activity;
-              return (
-                <li key={index}>
-                  {Time}: {Activity} ({Location.lat}, {Location.lng})
-                  <br />
-                  {Description}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      );
-    });
-
-    const locations = parsedResult.Itinerary.flatMap((item) =>
-      item.Activities.map((activity, index) => ({
-        id: index,
-        name: activity.Activity,
-        position: activity.Location,
-      }))
+    const itinerary = parseItinerary(result);
+    const allLocations = itinerary.flatMap((day) =>
+      day.Activities.map((activity) => activity.Location)
     );
-
-    const getMapCenter = (locations) => {
-      if (locations.length === 0) return defaultCenter;
-
-      const avgLatLng = locations.reduce(
-        (acc, loc) => {
-          acc.lat += loc.position.lat;
-          acc.lng += loc.position.lng;
-          return acc;
-        },
-        { lat: 0, lng: 0 }
-      );
-
-      avgLatLng.lat /= locations.length;
-      avgLatLng.lng /= locations.length;
-
-      return avgLatLng;
+    const mapCenter = {
+      lat:
+        allLocations.reduce((sum, loc) => sum + loc.lat, 0) /
+        allLocations.length,
+      lng:
+        allLocations.reduce((sum, loc) => sum + loc.lng, 0) /
+        allLocations.length,
     };
-
-    const mapCenter = getMapCenter(locations);
 
     return (
       <div className={styles.container}>
         <h1>Your Travel Itinerary</h1>
         <LoadScript googleMapsApiKey={API_KEY}>
-          <GoogleMap
-            mapContainerStyle={mapContainerStyle}
-            zoom={12}
-            center={mapCenter}
-          >
-            {locations.map((location) => (
-              <Marker
-                key={location.id}
-                position={location.position}
-                title={location.name}
-              />
+          <GoogleMap mapContainerStyle={mapContainerStyle} zoom={12} center={mapCenter}>
+            {allLocations.map((location, index) => (
+              <Marker key={index} position={location} />
             ))}
           </GoogleMap>
         </LoadScript>
-        <div className={styles.tiles}>{itineraryItems}</div>
+        <div className={styles.tiles}>
+          {itinerary.map((day, dayIndex) => (
+            <div key={dayIndex} className={styles.day}>
+              <h2>{day.Date}</h2>
+              <ul className={styles.itinerary}>
+                {day.Activities.map((activity, activityIndex) => (
+                  <li key={activityIndex}>
+                    {activity.Time}: {activity.Activity}
+                    <br />
+                    {activity.Description}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       </div>
     );
-            }}
+  } else {
+    return (
+      <div className={styles.container}>
+        <h1>Error: No itinerary data found.</h1>
+      </div>
+    );
+  }
+}
