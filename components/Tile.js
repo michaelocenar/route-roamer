3b9a3da9cd2af119f307ccb91edd6d63bca7a73d
namraaslam <namra.aslam@ucalgary.ca>
2023-04-19 19:01:52 -0600

    minor changes

--- components/Tile.js
+++ components/Tile.js
@@ -2,14 +2,14 @@ import Link from 'next/link';
 import Image from 'next/image';
 import styles from './Tile.module.css';
 
-const Tile = ({ href, src, alt, Text }) => {
+const Tile = ({ href, src, alt, text }) => {
   return (
-    <Link legacyBehavior href={""}>
+    <Link legacyBehavior href={href}>
       <a className={styles.tile}>
         <div className={styles.imageContainer}>
           <Image src={src} alt={alt} width={325} height={325} />
           <div className={styles.overlay}>
-          <p className={styles.overlayText}>{Text}</p>
+          <p className={styles.overlayText}>{text}</p>
           </div>
         </div>
       </a>
