a004983c0436c2c98153a389d987a4f649553347
michaelocenar <affectuminflamma@gmail.com>
2023-04-20 06:48:19 -0600

    minor changes to itinerary.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -61,11 +61,11 @@ function generatePrompt(destination, startDate, endDate, budget, preferences) {
   const { activities, accommodation, transportation } = preferences;
 
   return `Create a suggested itinerary for a trip to ${destination} from ${startDate} to ${endDate} with a budget of $${budget}. Return the itinerary in JSON format using the following structure:
-Include a label for each date i.e. Day 1, Day 2, etc.
   {
     "Itinerary": [
       {
         "Date": "Date of the activities",
+        "Label": "Include a label for each date i.e. Day 1, Day 2, etc."
         "Activities": [
           {
             "Time": "Suggested time for the activity in 12-hour format",
