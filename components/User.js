0a9e375c40016ea8de3f88ddb4203013cb97eb96
quinjenn <jquintal_@hotmail.com>
2023-04-19 19:51:53 -0700

    add div to User

--- components/User.js
+++ components/User.js
@@ -6,7 +6,10 @@ import styles from "/components/User.module.css";
 const User = () => {
   return (
     <div className={styles.User}>
-      <img src="https://static.vecteezy.com/system/resources/previews/010/054/157/original/chat-bot-robot-avatar-in-circle-round-shape-isolated-on-white-background-stock-illustration-ai-technology-futuristic-helper-communication-conversation-concept-in-flat-style-vector.jpg" className={styles.userImage} />
+      <span>
+        <img src="https://static.vecteezy.com/system/resources/previews/010/054/157/original/chat-bot-robot-avatar-in-circle-round-shape-isolated-on-white-background-stock-illustration-ai-technology-futuristic-helper-communication-conversation-concept-in-flat-style-vector.jpg" className={styles.userImage} />
+      </span>
+      <span><p>User Name: Route Robot</p></span>
     </div>
   );
 };
