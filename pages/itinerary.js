081506d0245cf18518238aee8ba97c6c5a0e4c65
michaelocenar <affectuminflamma@gmail.com>
2023-04-22 07:04:10 -0600

    style button

--- pages/itinerary.js
+++ pages/itinerary.js
@@ -4,6 +4,8 @@ import { GoogleMap, LoadScript, Marker, InfoWindow } from "@react-google-maps/ap
 import React, { useState, useRef } from "react";
 import { parseItinerary, API_KEY, mapContainerStyle, googleMapsLibraries } from "./itineraryHelpers";
 import Nav2 from "../components/Nav2";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 
 export default function Itinerary() {
   console.log("Itinerary component rendered");
@@ -20,6 +22,62 @@ export default function Itinerary() {
   const onUnmount = () => {
     mapRef.current = null;
   };
+
+  const downloadPDF = () => {
+    const itineraryElement = document.getElementById("itineraryContent");
+    const text = itineraryElement.innerText.split("\n");
+  
+    const pdf = new jsPDF("p", "pt", "a4"); // Set the orientation to portrait mode
+    const pageWidth = pdf.internal.pageSize.getWidth();
+    const pageHeight = pdf.internal.pageSize.getHeight();
+    const margin = 40; // You can adjust the margin as needed
+    const lineHeight = 20; // You can adjust the line height as needed
+    const maxLineWidth = pageWidth - 2 * margin;
+  
+    let y = margin;
+  
+    const unwantedWords = [
+      "Map",
+      "Satellite",
+      "Keyboard shortcuts",
+      "Terms of Use",
+      "Download Itinerary as PDF",
+    ];
+  
+    const filteredText = text.filter(
+      (line) => !unwantedWords.some((word) => line.includes(word))
+    );
+  
+    for (const line of filteredText) {
+      if (y > pageHeight - margin) {
+        pdf.addPage();
+        y = margin;
+      }
+  
+      if (line.startsWith("Day")) {
+        pdf.setFont("helvetica", "bold");
+        pdf.setFontSize(16);
+        y += lineHeight;
+      } else if (line.endsWith(":")) {
+        pdf.setFont("helvetica", "bold");
+        pdf.setFontSize(12);
+      } else {
+        pdf.setFont("helvetica", "normal");
+        pdf.setFontSize(12);
+      }
+  
+      const splitLines = pdf.splitTextToSize(line, maxLineWidth);
+  
+      for (const splitLine of splitLines) {
+        pdf.text(splitLine, margin, y);
+        y += lineHeight;
+      }
+    }
+  
+    pdf.save("itinerary.pdf");
+  };
+  
+  
   
   if (typeof result === "string") {
     console.log("Result is a string");
@@ -73,8 +131,15 @@ export default function Itinerary() {
       <div className={styles.darkModeWrapper}>
         <div className={styles.container}>
           <Nav2 />
-          <h1 className={styles.title}>Your Travel Itinerary</h1>
-          <div className={styles.content}>
+          <div className={styles.title}>
+            <h1>Your Travel Itinerary</h1>
+            <div className={styles.buttonDiv}>
+              <button onClick={downloadPDF} className={styles.downloadButton}>
+              Download PDF
+            </button>
+            </div>
+          </div>
+          <div id="itineraryContent" className={styles.content}>
             {typeof result === "string" ? (
             <LoadScript googleMapsApiKey={API_KEY} libraries={googleMapsLibraries}>
               <div className={styles.map}>
