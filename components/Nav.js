8cea0d9b74bfa4e40c1e6d17b78f2988c05ccb26
namraaslam <namra.aslam@ucalgary.ca>
2023-04-22 13:49:27 -0600

    fix nav bug

--- components/Nav.js
+++ components/Nav.js
@@ -1,19 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faUser, faRoute } from '@fortawesome/free-solid-svg-icons';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import styles from './Nav2.module.css';
+// import '@fortawesome/fontawesome-free/css/all.min.css';
+import styles from './Nav.module.css';
 import React from 'react';
 import Link from 'next/link';
 
-const Nav2 = () => {
+const Nav = () => {
   return (
     <div className={styles.nav}>
       <Link legacyBehaviour href="/">
         <img src="/nav-images/result.png" width={500} height={250} />
       </Link>
-      <nav>
+      <nav className={styles.fullNav}>
       <Link href="/">
-          <FontAwesomeIcon icon={faHouse} className={styles.icon} style={{ color: '#ebebeb', marginRight: '20px' }} size="2x" />
+          <FontAwesomeIcon icon={faHouse} className={styles.icon} style={{ color: '#ebebeb', marginRight: '20px' }} size="3x" />
         </Link>
         <Link href="/profile">
           <FontAwesomeIcon icon={faUser} className={styles.icon} style={{ color: '#ebebeb', marginRight: '20px'}} size="2x"/> 
@@ -26,4 +26,4 @@ const Nav2 = () => {
   );
 };
 
-export default Nav2;
+export default Nav;
--- components/Nav2.js
+++ components/Nav2.js
@@ -1,6 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faUser, faRoute } from '@fortawesome/free-solid-svg-icons';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import styles from './Nav2.module.css';
 import React from 'react';
 import Link from 'next/link';
@@ -11,9 +10,9 @@ const Nav2 = () => {
       <Link legacyBehaviour href="/">
         <img src="/nav-images/result.png" width={500} height={250} />
       </Link>
-      <nav>
-        <Link href="/">
-          <FontAwesomeIcon icon={faHouse} className={styles.icon} style={{ color: '#ebebeb', marginRight: '20px' }} size="2x" />
+      <nav className={styles.fullNav}>
+      <Link href="/">
+          <FontAwesomeIcon icon={faHouse} className={styles.icon} style={{ color: '#ebebeb', marginRight: '20px' }} size="3x" />
         </Link>
         <Link href="/profile">
           <FontAwesomeIcon icon={faUser} className={styles.icon} style={{ color: '#ebebeb', marginRight: '20px'}} size="2x"/> 
@@ -27,5 +26,3 @@ const Nav2 = () => {
 };
 
 export default Nav2;
-
-
