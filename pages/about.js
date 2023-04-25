44f5bb539fe3f01c8c2603d0d623dfdacb7e0cb0
namraaslam <namra.aslam@ucalgary.ca>
2023-04-25 13:51:54 -0600

    minor change to about page

--- pages/about.js
+++ pages/about.js
@@ -16,17 +16,17 @@ const About = () => {
         <div className={styles.namra}>
           <img src="/creators/NA.jpeg" className={styles.namraPic} />
           <h2 className={styles.title}>Namra Aslam</h2>
-          <p className={styles.description}>"Meet Namra! A full-stack developer from Calgary with a background in cybersecurity. During the project, Namra focused on integrating the Mapbox API as well as styling, ensuring a seamless user experience. When she's not coding, Namra can be found at a concert, binge-watching the latest HBO series on Sundays, on a long walk, or reading."</p>
+          <p className={styles.description}>Namra is a full-stack developer from Calgary with a background in cybersecurity. During the project, Namra focused on integrating the Mapbox API as well as styling, ensuring a seamless user experience. When she's not coding, Namra can be found at a concert, binge-watching the latest HBO series on Sundays, on a long walk, or reading.</p>
         </div>
         <div className={styles.michael}>
           <img src="/creators/MO1.jpeg" className={styles.michaelPic} />
           <h2 className={styles.title}>Michael Ocenar</h2>
-          <p className={styles.description}>"Michael is a full-stack developer from Calgary with prior experience in accounting/finance. His part in the project dealt mainly with the integration of the OpenAI and Google Maps API. When he isn't programming, Michael loves to ski, hike, bike, play the piano, and travel."</p>
+          <p className={styles.description}>Michael is a full-stack developer from Calgary with prior experience in accounting/finance. His part in the project dealt mainly with the integration of the OpenAI and Google Maps API. When he isn't programming, Michael loves to ski, hike, bike, play the piano, and travel.</p>
         </div>
         <div className={styles.jennifer}>
           <img src="/creators/JQ.jpeg" className={styles.jenniferPic} />
           <h2 className={styles.title}>Jennifer Quintal</h2>
-          <p className={styles.description}>"Jennifer is a full-stack web developer with a background in operations and experiential marketing. Her main focus on Route Roamer was providing users with suggested itineraries, creating user navigation, along with minor styling. When Jennifer is not programming, you can find her running on the seawall, in a yoga studio, or at one of Vancouver's fine beaches."</p>
+          <p className={styles.description}>Jennifer is a full-stack web developer with a background in operations and experiential marketing. Her main focus on Route Roamer was providing users with suggested itineraries, creating user navigation, along with minor styling. When Jennifer is not programming, you can find her running on the seawall, in a yoga studio, or at one of Vancouver's fine beaches.</p>
         </div>
       </div>
     </div>
