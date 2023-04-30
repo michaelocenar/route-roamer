2c0ae59f159016b37a063a34dd81e1a58db50b02
michaelocenar <affectuminflamma@gmail.com>
2023-04-29 22:37:27 -0600

    testing without nav and tile

--- components/Tile.js
+++ components/Tile.js
@@ -1,31 +1,31 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import styles from './Tile.module.css';
-import { useState } from 'react';
+// import Image from 'next/image';
+// import Link from 'next/link';
+// import styles from './Tile.module.css';
+// import { useState } from 'react';
 
-const Tile = ({ href, src, alt, text }) => {
-  const [isFavourite, setIsFavourite] = useState(false);
+// const Tile = ({ href, src, alt, text }) => {
+//   const [isFavourite, setIsFavourite] = useState(false);
 
-  const toggleFavourite = () => {
-    setIsFavourite(!isFavourite);
-  };
+//   const toggleFavourite = () => {
+//     setIsFavourite(!isFavourite);
+//   };
 
-  return (
-    <div className={styles.tile}>
-      <Link href={href}>
-        <div className={styles.imageContainer}>
-          <Image src={src} alt={alt} width={325} height={325} />
-        </div>
-      </Link>
-      <div className={styles.overlay}>
-        <p className={styles.overlayText}>{text}</p>
-      </div>
-      <div className={styles.favouriteButton} onClick={toggleFavourite}>
-        <div className={styles.favouriteButtonInner}></div>
-        {isFavourite ? '❤️' : '🤍'}
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className={styles.tile}>
+//       <Link href={href}>
+//         <div className={styles.imageContainer}>
+//           <Image src={src} alt={alt} width={325} height={325} />
+//         </div>
+//       </Link>
+//       <div className={styles.overlay}>
+//         <p className={styles.overlayText}>{text}</p>
+//       </div>
+//       <div className={styles.favouriteButton} onClick={toggleFavourite}>
+//         <div className={styles.favouriteButtonInner}></div>
+//         {isFavourite ? '❤️' : '🤍'}
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Tile;
+// export default Tile;
