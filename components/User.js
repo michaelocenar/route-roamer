6b87ed3eef31dc234f660e6da37f5f3244cae423
michaelocenar <affectuminflamma@gmail.com>
2023-04-29 21:29:59 -0600

    change syntax for react import in user.js

--- components/User.js
+++ components/User.js
@@ -1,4 +1,4 @@
-import { React, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import styles from "/components/User.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
